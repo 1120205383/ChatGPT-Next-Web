977665b212a59034b5c3bcbbc56b3025eaf04cc3
1120205383 <130739801+1120205383@users.noreply.github.com>
2023-04-15 19:21:23 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -383,7 +383,19 @@ export function Settings(props: { closeSettings: () => void }) {
           )}
 
           
-        
+         <SettingItem
+            title={Locale.Settings.Token.Title}
+            subTitle={Locale.Settings.Token.SubTitle}
+          >
+            <PasswordInput
+              value={accessStore.token}
+              type="text"
+              placeholder={Locale.Settings.Token.Placeholder}
+              onChange={(e) => {
+                accessStore.updateToken(e.currentTarget.value);
+              }}
+            />
+          </SettingItem>
           
           
           
